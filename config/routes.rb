c01c36c5a4bc23ae169360258a2f542941a75dd2
rory jackson <roryjamesjackson@hotmail.com>
2022-11-16 18:04:06 +0000

    formatting of main page and added most links

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get "featured", to: "pages#featured"
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
